aea15456fda1660d2cd3b96891297e5eccd1fdbe
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2022-05-04 18:02:06 +0100

    reformat

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,44 +1,44 @@
 export interface Data {
-    [key: string]: any;
+	[key: string]: any;
 }
 
 export type Prediction = (data: Data) => boolean;
 
 export declare interface CakebaseProvider {
-    /**
-     * Add an object to the json file.
-     * @param data the object.
-     */
-    set(data: Data): Promise<void>;
+	/**
+	 * Add an object to the json file.
+	 * @param data the object.
+	 */
+	set(data: Data): Promise<void>;
 
-    /**
-     * Retrieve for objects in the json file
-     * @param prediciton the prediction to find the objects.
-     */
-    get(prediciton: Prediction): Promise<Data[]>;
+	/**
+	 * Retrieve for objects in the json file
+	 * @param prediciton the prediction to find the objects.
+	 */
+	get(prediciton?: Prediction): Promise<Data[]>;
 
-    /**
-     * Remove objects in the json file
-     * @param prediciton the prediction to find the objects
-     */
-    remove(prediciton: Prediction): Promise<void>;
+	/**
+	 * Remove objects in the json file
+	 * @param prediciton the prediction to find the objects
+	 */
+	remove(prediciton: Prediction): Promise<void>;
 
-    /**
-     * Update objects in the json file
-     * @param prediciton the prediction to find the objects
-     * @param data Data you want to edit or add
-     */
-    update(prediciton: Prediction, data: Data): Promise<void>;
+	/**
+	 * Update objects in the json file
+	 * @param prediciton the prediction to find the objects
+	 * @param data Data you want to edit or add
+	 */
+	update(prediciton: Prediction, data: Data): Promise<void>;
 
-    /**
-     * Delete all objects in the json file
-     */
-    clear(): Promise<void>;
+	/**
+	 * Delete all objects in the json file
+	 */
+	clear(): Promise<void>;
 }
 
 /**
  * Cakebase is an asynchronous fast json database.
- * 
+ *
  * Initalize the json database.
  * @param root The json file path where our objects are stored.
  */
